7483c3de6709d8c844b2c4d2a9f0b9a464a9e854
downtowner <downtowner@users.noreply.github.com>
2020-11-30 11:33:10 +0800

    Create ziphelper_test.go

---
+++ functional/ziphelper/ziphelper_test.go
@@ -0,0 +1,29 @@
+package ziphelper
+
+import (
+	"io/ioutil"
+	"log"
+	"testing"
+)
+
+func TestZip(t *testing.T) {
+
+	zipHelper := NewZipHelper()
+	zipHelper.Add("1.bin", []byte("1test2test3test4test"))
+	zipHelper.Add("2.json", []byte("I have nothing in the world"))
+	data, err := zipHelper.Compress()
+	if nil != err {
+
+		log.Println("zip err:", err)
+		return
+	}
+
+	ioutil.WriteFile("1.zip", data, 0644)
+
+	files, _ := zipHelper.Uncompress(data)
+
+	for _, v := range files {
+
+		log.Println(string(v))
+	}
+}
